dcea4d4f891e713ff6791e963583a7546e7811fc
clarkorcullo <lr.corcullo@mmdc.mcl.edu.ph>
2025-09-26 09:33:06 +0800

    Initial commit: Fresh start with cleaned project

---
+++ learning_modules/__init__.py
@@ -0,0 +1,7 @@
+"""
+Modules package for Social Engineering Awareness Program
+Content will be recreated fresh
+"""
+
+# All module content has been cleared for fresh start
+__all__ = []
---
+++ simulations/__init__.py
@@ -0,0 +1,7 @@
+"""
+Simulations package for Social Engineering Awareness Program
+Content will be recreated fresh
+"""
+
+# All simulation content has been cleared for fresh start
+__all__ = []
